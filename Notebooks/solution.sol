97dc607f63b9a099a339f8502a2bac6d1ec0dc3f
Luis Correia <lacorreia65@terra.com.br>
2025-09-30 00:34:50 -0300

    Initial commit

---
+++ Notebooks/solution.sol
@@ -0,0 +1,14 @@
+# Solution for model steel_beams
+# Objective value = 3960.07326007326
+h[p,A] 0
+h[p,B] 1.8461538461538460e+01
+h[p,C] 0
+h[m,A] 0
+h[m,B] 0
+h[m,C] 8.5714285714285712e+00
+h[g,A] 0
+h[g,B] 0
+h[g,C] 8.3333333333333339e+00
+h[e,A] 50
+h[e,B] 0
+h[e,C] 2.3076923076923075e+00
